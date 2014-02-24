18948ccd7305515d04e6c276a73da10fe3a75518
Chris Bednarski <banzaimonkey@gmail.com>
2014-02-23 22:30:32 -0800

    Added homepage controller

--- src/funnul.py
+++ src/funnul.py
@@ -2,7 +2,9 @@ import flask
 
 app = flask.Flask(__name__)
 
-
+@app.route('/')
+def homepage():
+    return flask.render_template('applications.html')
 
 if __name__ == '__main__':
     app.debug = True
