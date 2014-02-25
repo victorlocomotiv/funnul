27064a7ea7487cb8fc4480311bf64dc66d299a8a
Chris Bednarski <banzaimonkey@gmail.com>
2014-02-25 09:22:14 -0800

    Added some mockups and updated placeholder templates

--- src/control.py
+++ src/control.py
@@ -4,4 +4,8 @@ ctl = flask.Blueprint('control', __name__, url_prefix='/control')
 
 @ctl.route('/')
 def homepage():
-    return flask.render_template('applications.html')
+    return flask.render_template('app_list.html')
+
+@ctl.route('/app/<name>')
+def application(name):
+    return flask.render_template('app_config.html', name=name)
